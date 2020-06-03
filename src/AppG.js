913d450b6a7b773f0c92f62b187b863de6bbb25d
Victor-28 <victorakintola@gmail.com>
2020-06-03 14:33:43 +0200

    making my app

---
+++ src/AppG.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import Garage from " ./Garage.js"
+import './App.css';
+ class App extends Component{
+     render (){
+         return(
+         <div  className="App">
+             <h1>APP</h1>h>
+             </div>
+         );
+     }
+ }
---
+++ src/Garage.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import Garage from " ./Garage.js"
+import './App.css';
+
+class Garage extends Component{
+    render (){
+        return(
+            <div  className="App">
+                <h1>Garage</h1>
+                <Garage/>
+            </div>
+        );
+    }
+}
+export default app;
