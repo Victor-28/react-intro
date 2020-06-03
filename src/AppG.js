b2ec54bd260815f09c3eaaee30c00903260328a8
Victor-28 <victorakintola@gmail.com>
2020-06-03 16:31:15 +0200

    prop

--- src/AppG.js
+++ src/AppG.js
@@ -1,12 +1,42 @@
 import React, {Component} from 'react';
 import Garage from " ./Garage.js"
 import './App.css';
+
  class App extends Component{
-     render (){
-         return(
-         <div  className="App">
-             <h1>APP</h1>h>
-             </div>
-         );
-     }
+     state={
+         Garage: [
+
+             {
+
+                 id: 1,
+                 title: 'scan the car',
+                 completed: false
+             },
+
+             {
+                 id: 2,
+                 title: 'control the car',
+                 completed: false
+             },
+
+             {
+                 id: 3,
+                 title: 'display car of the week',
+                 completed: false
+
+             },
+
+                ]
  }
+             render() {
+           console.log (this.states.Garage)
+             return(
+                 <div className="App">
+                     <Garage garage={ this.state.garage} />
+                 </div>
+             );
+             }
+ }
+
+
+     export default App;
