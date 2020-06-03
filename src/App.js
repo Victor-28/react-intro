b1bbb5818d7c1115e9440d063fb261968a436185
Victor-28 <victorakintola@gmail.com>
2020-06-03 11:06:05 +0200

    export

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Car from  "./Car.js"
+
+function App() {
+  return (
+
+
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Hello World React</h2>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+          <p>My first React, Am here to learn how you work</p>
+
+          <table>
+            <tr>
+              <th>Name</th>
+            </tr>
+            <tr>
+              <td>Dave</td>
+            </tr>
+            <tr>
+              <td>Elsa</td>
+            </tr>
+            <tr>
+              <td>Mateo</td>
+            </tr>
+          </table>
+
+        </p>
+        <Car/>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Car.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Car extends React.Component {
+  render() {
+    return <h2>Hi, I am a Car named American Muscle!</h2>;
+  }
+}
+
+export default Car;
