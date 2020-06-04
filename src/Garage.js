ce6590810d8e6f9bc5389b639ced575e09d0c0ee
Victor-28 <victorakintola@gmail.com>
2020-06-04 15:48:29 +0200

    importing

--- src/Garage.js
+++ src/Garage.js
@@ -1,14 +1,18 @@
 import React, {Component} from 'react';
 import GarageV from'./GarageV';
+import PropTypes from 'prop-type';
 
 class Garages extends Component {
     render() {
         return this.props.garage.map((garage) => (
             <GarageV key={Garage.id} Garage={Garage}/>
 
-            
+
 
         ));
     }
+}
+Garages.propTypes = {
+    garages: PropTypes.array.IsRequired
 }
 export default Garages;
--- src/GarageV.js
+++ src/GarageV.js
@@ -1,9 +1,19 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-type';
 class Garages extends Component {
     render() {
         return this.props.garage.map((garage) => (
+        <div>
+            <p>{this.prop.Garage.title}</p>
+        </div>
             <h2>Hello</h2>
         ));
     }
 }
+Garages.propTypes = {
+    garages: PropTypes.array.IsRequired
+}
+
+
+
 export default Garages;
